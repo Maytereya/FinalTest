3e4a259db6bbd67069c9944da93c984d41d0e9ee
Vladimir <rakhmanov-v@ya.ru>
2023-05-30 23:48:40 +0400

    Упрощен код

--- final/Program.cs
+++ final/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите строковые элементы массива: ");
+
+string? input = Console.ReadLine();
+
+string[] array = input.Split(' ');
+
+Console.WriteLine($"Массив строк: [{string.Join(",", array)}]");
+
+int newarrlen = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newarrlen++;
+    }
+}
+Console.WriteLine($"Длина нового массива:{newarrlen}");
+
+string[] newarr = new string[newarrlen];
+int l = 0;
+for (int s = 0; s < array.Length; s++)
+{
+    if (array[s].Length <= 3)
+    {
+        newarr[l] = array[s];
+        l++;
+    }
+}
+Console.WriteLine($"Массив строк c учетом условия задачи: [{string.Join(",", newarr)}]");
