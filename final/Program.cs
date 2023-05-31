50ab15473794371267eb81ba71ced921ca76c0f7
Vladimir <rakhmanov-v@ya.ru>
2023-05-31 15:46:04 +0400

    Правки в коде

--- final/Program.cs
+++ final/Program.cs
@@ -4,23 +4,21 @@ Console.Clear();
 Console.Write("Введите строковые элементы массива: ");
 
 string? input = Console.ReadLine();
-
 string[] array = input.Split(' ');
 
 Console.WriteLine($"Массив строк: [{string.Join(",", array)}]");
 
-int newarrlen = 0;
+int newarrlen = 0, l = 0;
+
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i].Length <= 3)
-    {
         newarrlen++;
-    }
 }
 Console.WriteLine($"Длина нового массива:{newarrlen}");
 
 string[] newarr = new string[newarrlen];
-int l = 0;
+
 for (int s = 0; s < array.Length; s++)
 {
     if (array[s].Length <= 3)
